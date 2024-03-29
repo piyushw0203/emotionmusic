483a5f65fc95876f080e51ceaae06e87b46ab82b
Piyush Waghulde <piyushw0203@gmail.com>
2024-03-29 20:05:47 +0530

    changed layout a bit

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ st.write("First of all, welcome! This is the place where you can customize what 
 st.markdown("##")
 
 with st.container():
-    col1, col2, col3, col4 = st.columns((2, 0.5, 0.5, 0.5))
+    col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
     with col3:
         st.markdown("***Choose your genre:***")
         genre = st.radio("Choose your genre:", genre_names, index=genre_names.index("Pop"), label_visibility='hidden')
@@ -135,7 +135,7 @@ with st.container():
         selected_emotion = st.selectbox('Choose an emotion:', list(emotion_presets.keys()) + ["Start Camera"], index=list(emotion_presets.keys()).index('Neutral'), label_visibility='hidden')
         
         if selected_emotion == "Start Camera":
-            st.write("Starting camera for emotion detection...")
+            st.write("Starting camera for emotion detection")
             webrtc_streamer(key="example", video_processor_factory=lambda: video_transformer)
             if st.button('Stop'):
                 try:
